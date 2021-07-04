6110c3a2f5ffd555233f1eb9b621605fb1c20d5c
lexer3 <lpreciado617@gmail.com>
2021-07-03 17:15:30 -0700

    Update colaborador1.h

--- colaborador1.h
+++ colaborador1.h
@@ -1,8 +1,54 @@
+// esta parte me toco a mi Lexer Preciado aqui se llenaran las matrices con los valores correspondientes antes de hacer uso de cualquier operacion
+
+
 void ingresar(float (*m1)[5], float (*m2)[5])
 {
 
 cout<<"Ingresar y mostrar los valores de los arreglos o matrices";
-
+//PARTICIPANTE 1//
+	cout<<"EN ESTA PARTE PODREMOS PONER LOS VALORES DE LAS MATRICES\n";
+	cout<<"\nPOR FAVOR DIGITE LOS NUMEROS DE LA PRIMERA MATRIZ\n";
+	for(int a=0;a<2;a++)
+	{
+		for(int b=0;b<2;b++)
+		{
+			cout<<"\nINGRESE UN NUMERO ["<<a<<"]["<<b<<"]: ";
+			cin>>matriz1[a][b];
+		}
+	}
+	
+	cout<<"\nPOR FAVOR DIGITE LOS NUMEROS DE LA SEGUNDA MATRIZ\n";
+	for(int a=0;a<2;a++)
+	{
+		for(int b=0;b<2;b++)
+		{
+			cout<<"\nINGRESE UN NUMERO ["<<a<<"]["<<b<<"]: ";
+			cin>>matriz2[a][b];
+		}
+	}
+	
+	cout<<"\nESTA ES LA PRIMERA MATRIZ\n";
+	for(int a=0;a<2;a++)
+	{
+		for(int b=0;b<2;b++)
+		{
+			cout<<matriz1[a][b]<<" ";
+		}
+		cout<<"\n";
+	}
+	
+	cout<<"\nESTA ES DE LA SEGUNDA MATRIZ\n";
+	for(int a=0;a<2;a++)
+	{
+		for(int b=0;b<2;b++)
+		{
+			cout<<matriz2[a][b]<<" ";
+		}
+		cout<<"\n";
+	}
+		break;
+		
+	        case 2:
 
 
 
