94408cf20d3c25d071a9a6d06514db9232a72dd6
Alissonlucas18 <86218134+Alissonlucas18@users.noreply.github.com>
2021-07-03 20:47:23 -0500

    Update colaborador5.h
    Se procedió a realizar la media y varianza de las dos matrices

--- colaborador5.h
+++ colaborador5.h
@@ -1,9 +1,73 @@
 void mediayvarianza(float (*m1)[5], float (*m2)[])
-{
 
-cout<<"Determinar  cual de las 2 matrices tiene la mayor media y varianza";
+//==========================================================================
+//AQUI VOY A CALCULAR Y MOSTRAR LA MEDIA Y VARIANZA DE LAS DOS MATRICES
+  
+  for(int a=0;a<2;a++)
+  {
+     for(int b=0;b<2;b++)
+     {
+      matrizresultado[a][b]=0;
+      matrizresultado[a][b]=matrizresultado[a][b]+matriz1[a][b]*matriz2[a][b];
+      matrizresultado[a][b]=matrizresultado[a][b]/2;
+     }
+  }  
+  
+  for(int a=0;a<2;a++)
+  {
+     for(int b=0;b<2;b++)
+     {
+      matrizresultado1[a][b]=0;
+      matrizresultado1[a][b]=matrizresultado[a][b]+matriz1[a][b]*matriz2[a][b];
+      matrizresultado1[a][b]=matrizresultado[a][b]*2;
+     }
+  }  
 
 
+     cout<<"\nLA MEDIA ARITMETICA ES:\n;
+       
+  for(int a=0;a<2;a++)
+  {
+     for(int b=0;b<2;b++)
+     {
+        cout<<matrizresultado[a][b]<<" ";
+     }
+    
+    cout<<"\n";  
+  }
+
+
+     cout<<"\nLA VARIANZA ARITMETICA, QUEDARIA ASI:\n";
+ 
+   for(int a=0;a<2;a++)
+  {
+     for(int b=0;b<2;b++)
+     {
+        cout<<matrizresultado1[a][b]<<" ";
+     }
+     cout<<"\n";  
+  }
+
+     break;
 
 
 }
+
+     if(op<0 || op>5)
+     {
+        cout<<"\nEste numero es valido..\n";
+       system("cls");  
+     }
+
+
+   if (op==0)
+   {
+      cout<<"\nLEXER PRECIADO, WENDY ROBINZON, ALISSON LUCAS..\n";
+       cout<<"TERMINAR: "<<endl;
+   }
+
+}
+   while(op!=0);
+   return(0);
+}
+
