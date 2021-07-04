ce6bb83a643f72ef0638ff490482985de9dd5fbb
Alissonlucas18 <86218134+Alissonlucas18@users.noreply.github.com>
2021-07-03 20:02:50 -0500

    Update colaborador4.h
    Alisson Lucas, lo que me toco realizar fue la transpuesta de la primera matriz

--- colaborador4.h
+++ colaborador4.h
@@ -1,9 +1,23 @@
 void transpuesta(float (*m1)[5], float (*r)[5])
-{
-
-cout<<"Obtener la transpuesta de una matriz";
-
-
-
-
+  
+//ALISSON LUCAS 
+//A MI ME TOCO CALCULAR LA TRANSPUESTA DE LAS  MATRICES 
+  
+//AQUI SE VA A CALCULAR LA TRANSPUESTA DE LA MATRIZ 1 
+  
+cout<<"\nLA PRIMERA MATRIZ, QUEDARIA ASI:\n";
+  
+for(int a=0; a<2; a++)
+{  
+    for(int b=0; b<2; b++)
+    {
+    cout<<matriz1[b][a]<<" ";
+    }
+    
+    cout<<"\n";    
 }
+
+   break;
+
+
+   case 5:
