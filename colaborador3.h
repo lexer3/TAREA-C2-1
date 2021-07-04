5ee8ed9d5e44cb2b50da42f7ffcfefdde53917a3
wendyrobinzon <86172748+wendyrobinzon@users.noreply.github.com>
2021-07-03 21:32:36 -0500

    Update colaborador3.h
    A MI ME TOCO SACAR LA POTENCIA DE LAS MATICES

--- colaborador3.h
+++ colaborador3.h
@@ -7,3 +7,32 @@ cout<<"Calcular la potencia p de una ";
 
 
 }
+//=====================================================
+//COLABORADOR 3
+//WENDY ROBINZON 
+//A MI ME TOCO SACAR LA POTENCIA DE LAS MATRICES
+    //AQUI VAMOS A OBTENER LA POTENCIA DE UNA MATRIZ.
+
+    for (int a=0;a<2;a++)
+    {
+    for (int b=0;b<2;b++)
+         {
+          matrizresultado[a][b]=0;
+          matrizresultado[a][b]=matriz resultado[a][b]+matriz1[a][b]*matriz1[a][b];
+         }
+    }
+    Cout<<"\nPARA LA POTENCIA DE LA MATRIZ 1,ES:\n";
+
+    for(int a=0;a<2;a++)
+    {
+        for(int b=0;b<2;b++)
+        {
+            Cout<<matriz resultado[a][b]<<" ";
+        }
+        Cout<<"\n";
+    }
+
+        break;
+
+          
+    
