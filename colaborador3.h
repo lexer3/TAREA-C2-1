d4a30e90629fb31fb3c9cd09d6fae1f2f274b269
wendyrobinzon <86172748+wendyrobinzon@users.noreply.github.com>
2021-07-03 21:44:48 -0500

    Create colaborador3.h
    A MI ME TOCO SACAR LA POTENCIA DE LAS MATICES

--- colaborador3.h
+++ colaborador3.h
@@ -1,32 +1,38 @@
 void potencia(float (*m1)[5], int p, float (*r)[5])
+{
 
+cout<<"Calcular la potencia p de una ";
+
+
+
+
+}
 //=====================================================
-//COLABORADOR 3 
+//COLABORADOR 3
 //WENDY ROBINZON 
 //A MI ME TOCO SACAR LA POTENCIA DE LAS MATRICES
-    //AQUI VAMOS A OBTENER LA POTENCIA DE UNA MATRIZ
+    //AQUI VAMOS A OBTENER LA POTENCIA DE UNA MATRIZ.
 
     for (int a=0;a<2;a++)
     {
-         for (int b=0;b<2;b++)
+    for (int b=0;b<2;b++)
          {
           matrizresultado[a][b]=0;
-          matrizresultado[a][b]=matrizresultado[a][b]+matriz1[a][b]*matriz1[a][b];
+          matrizresultado[a][b]=matriz resultado[a][b]+matriz1[a][b]*matriz1[a][b];
          }
     }
-    cout<<"\nPARA LA POTENCIA DE LA MATRIZ 1,ES:\n";
+    Cout<<"\nPARA LA POTENCIA DE LA MATRIZ 1,ES:\n";
 
     for(int a=0;a<2;a++)
     {
         for(int b=0;b<2;b++)
         {
-            cout<<matrizresultado[a][b]<<" ";
+            Cout<<matriz resultado[a][b]<<" ";
         }
-        cout<<"\n";
+        Cout<<"\n";
     }
 
         break;
 
           
-case 4:
-//=====================================================
+    
